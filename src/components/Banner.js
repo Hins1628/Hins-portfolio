6dee335079e47a5984ff53f7ca0aa0ede66fcb7a
Thomas Lam <HinsLam866@gmail.com>
2024-11-22 03:30:06 +0800

    Update title

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ import TrackVisibility from 'react-on-screen';
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Computing Student", "Web Developer"];
+    const toRotate = ["Full-Stack Engineer", "Web Developer"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);    
     const period = 2000;
